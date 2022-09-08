4be9727e0873a7973ac46ee3c1007ac7c8b55827
Tharunraj Optimus <tharunoptimus@outlook.com>
2022-09-08 17:27:34 +0000

    Updated the stack

--- pages/components/projectData.ts
+++ pages/components/projectData.ts
@@ -1,4 +1,4 @@
-import { html, css, js, mongodb, express, nodejs, pwa } from "./stackData"
+import { html, css, js, mongodb, express, nodejs, pug, pwa, jquery, bootstrap, react, ts, vscode, npm, go, nextjs } from "./stackData"
 import { CardProps } from "./card"
 
 export let fullStack: CardProps[] = [
@@ -6,7 +6,7 @@ export let fullStack: CardProps[] = [
         imageUrl: "/assets/images/pics/meowit.webp",
         cardTitle: "Meow It - Find your clowder!",
         cardDescription: "Anonymous Location based Social Media. Safe for children.",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [html, css, js, express, mongodb, pwa],
         projectURL: "https://meowit.pages.dev",
         repoURL: "https://github.com/meow-it"
     },
@@ -14,7 +14,7 @@ export let fullStack: CardProps[] = [
         imageUrl: "/assets/images/pics/talk.png",
         cardTitle: "Talk Messenger",
         cardDescription: "Anonymous private messenger to protect people 's privacy while they chat with their friends.",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [html, css, js, jquery, express, mongodb, nodejs, pwa],
         projectURL: "https://disposeable.herokuapp.com",
         repoURL: "https://github.com/tharunoptimus/talk"
     },
@@ -22,7 +22,7 @@ export let fullStack: CardProps[] = [
         imageUrl: "/assets/images/pics/thunderme.png",
         cardTitle: "Thunder Me",
         cardDescription: "A social media website for sharing your thoughts and experiences. Built entirely with RESTful APIs.",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [pug, css, bootstrap, js, jquery, express, mongodb, nodejs, pwa],
         projectURL: "https://thunderme.herokuapp.com",
         repoURL: "https://github.com/tharunoptimus/thunderme"
     },
@@ -30,7 +30,7 @@ export let fullStack: CardProps[] = [
         imageUrl: "/assets/images/pics/classroom.png",
         cardTitle: "Classroom",
         cardDescription: "Online classroom management system for students to manage their lectures, messages, and assignments.",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [pug, css, bootstrap, js, jquery, express, mongodb, nodejs, pwa],
         projectURL: "https://classroom-task.herokuapp.com",
         repoURL: "https://github.com/tharunoptimus/classroom"
     },
@@ -38,7 +38,7 @@ export let fullStack: CardProps[] = [
         imageUrl: "/assets/images/pics/search.png",
         cardTitle: "New Search",
         cardDescription: "A scalable search engine built with Express.js and MongoDB which shows sites and images based on the search query.",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [pug, css, js, jquery, express, mongodb, pwa],
         projectURL: "https://newsea.herokuapp.com",
         repoURL: "https://github.com/tharunoptimus/classroom"
     },
@@ -46,7 +46,7 @@ export let fullStack: CardProps[] = [
         imageUrl: "/assets/images/pics/paint.png",
         cardTitle: "Paint",
         cardDescription: "A shareable Paint App to showcase your creativity. Share, Save, Edit and Delete at any time. Now available with multiple templates to begin with.",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [pug, css, js, express, mongodb, nodejs, pwa],
         projectURL: "https://newpaint.herokuapp.com",
         repoURL: "https://github.com/tharunoptimus/classroom"
     },
@@ -57,7 +57,7 @@ export let pwaspa: CardProps[] = [
         imageUrl: "/assets/images/pics/coolsw.png",
         cardTitle: "Cool SW",
         cardDescription: "Generate a 😎 Service Worker in an Instant ⚡ - Batteries included",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [html, css, js, pwa],
         projectURL: "https://coolsw.pages.dev",
         repoURL: "https://github.com/tharunoptimus/coolsw"
     },
@@ -65,7 +65,7 @@ export let pwaspa: CardProps[] = [
         imageUrl: "/assets/images/pics/weather.png",
         cardTitle: "Weather App",
         cardDescription: "My first PWA app. It shows the current weather of a city. It uses OpenWeatherMap API.",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [react, pwa],
         projectURL: "https://findcurrentweather.netlify.app",
         repoURL: "https://github.com/tharunoptimus/weather"
     },
@@ -73,7 +73,7 @@ export let pwaspa: CardProps[] = [
         imageUrl: "/assets/images/pics/grocery.png",
         cardTitle: "Grocery App",
         cardDescription: "I often forget things to buy in the grocery store. This app helps me remember them. Runs Offline 📡 with IndexedDB",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [html, css, js, jquery, pwa],
         projectURL: "https://idb-grocery.netlify.app",
         repoURL: "https://github.com/tharunoptimus/grocery"
     },
@@ -81,7 +81,7 @@ export let pwaspa: CardProps[] = [
         imageUrl: "/assets/images/pics/arkanoid.png",
         cardTitle: "Arkanoid",
         cardDescription: "I love playing this game so I built one with TypeScript.",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [react, ts],
         projectURL: "https://arkanoid-ts.netlify.app",
         repoURL: "https://github.com/tharunoptimus/arkanoid"
     },
@@ -89,7 +89,7 @@ export let pwaspa: CardProps[] = [
         imageUrl: "/assets/images/pics/short.png",
         cardTitle: "URL Shortner",
         cardDescription: "URL Shortner built with Express.js RESTful API and MongoDB. It allows users to shorten their URLs.",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [html, css, js, jquery, pwa],
         projectURL: "https://shortthis.herokuapp.com",
         repoURL: "https://github.com/tharunoptimus/short"
     },
@@ -97,7 +97,7 @@ export let pwaspa: CardProps[] = [
         imageUrl: "/assets/images/pics/crawler.png",
         cardTitle: "New Search Crawler",
         cardDescription: "Crawler bot to crawl and scrap data from websites. New Search uses this crawler!",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [html, css, js, jquery, pwa],
         projectURL: "https://newseacrawler.herokuapp.com",
         repoURL: "https://github.com/tharunoptimus/crawlerbot"
     },
@@ -108,7 +108,7 @@ export let apis: CardProps[] = [
         imageUrl: "/assets/images/pics/css.png",
         cardTitle: "Convert CSS Units",
         cardDescription: "This extension helps you convert CSS units to pixels and vice versa. Has support for 13 different CSS Units.",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [vscode, js],
         projectURL: "https://github.com/tharunoptimus/css-units/releases/tag/1.0.0",
         repoURL: "https://github.com/tharunoptimus/css-units"
     },
@@ -116,7 +116,7 @@ export let apis: CardProps[] = [
         imageUrl: "/assets/images/pics/placeholder.png",
         cardTitle: "Placeholder Images API",
         cardDescription: "Generate Placeholder Images for a given height and width. Includes support for text and color.",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [npm, express, nodejs],
         projectURL: "https://placeholder-image-api.herokuapp.com/",
         repoURL: "https://github.com/tharunoptimus/placeholder-image-data-url"
     },
@@ -132,7 +132,7 @@ export let apis: CardProps[] = [
         imageUrl: "/assets/images/pics/qr.png",
         cardTitle: "Generate QR API",
         cardDescription: "Generate QR Codes for a given text or URL for a given size in width and height. Supports multiple formats. Simply specify the data parameter in the URL",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [npm, express, nodejs],
         projectURL: "https://qrapigen.herokuapp.com/?data=meow",
         repoURL: "https://github.com/tharunoptimus/qrcode-generator-api.git"
     }
@@ -143,7 +143,7 @@ export let cli: CardProps[] = [
         imageUrl: "/assets/images/pics/npm.png",
         cardTitle: "crypto-string",
         cardDescription: "Generate crypto random string that can be used for chat rooms and other id specific use case. Install with npm.",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [npm, js],
         projectURL: "https://www.npmjs.com/package/crypto-string",
         repoURL: "https://github.com/tharunoptimus/crypto-string"
     },
@@ -151,7 +151,7 @@ export let cli: CardProps[] = [
         imageUrl: "/assets/images/pics/endict.jpg",
         cardTitle: "Endict",
         cardDescription: "Command line English dictionary. Search for words and get the meaning. Install with npm.",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [npm, js],
         projectURL: "https://www.npmjs.com/package/endict",
         repoURL: "https://github.com/tharunoptimus/endict"
     },
@@ -159,7 +159,7 @@ export let cli: CardProps[] = [
         imageUrl: "/assets/images/pics/findodd.jpg",
         cardTitle: "Find the sus word!",
         cardDescription: "Command line game to find the word which is not related to the given word. Install with npm.",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [npm, js],
         projectURL: "https://www.npmjs.com/package/find-the-odd",
         repoURL: "https://github.com/tharunoptimus/find-the-odd"
     },
@@ -167,7 +167,7 @@ export let cli: CardProps[] = [
         imageUrl: "/assets/images/pics/npm.png",
         cardTitle: "Placeholder Images API NPM Package",
         cardDescription: "The npm package for Placeholder Images API.",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [npm, js],
         projectURL: "https://www.npmjs.com/package/placeholder-image-data-url",
         repoURL: "https://github.com/tharunoptimus/placeholder-image-data-url"
     },
@@ -175,7 +175,7 @@ export let cli: CardProps[] = [
         imageUrl: "/assets/images/pics/infant.webp",
         cardTitle: "Infant Lang",
         cardDescription: "The Esoteric Programming Language for Infants",
-        stack: [html, css, js, express, mongodb, nodejs, pwa],
+        stack: [nextjs, go, pwa],
         projectURL: "https://infant.pages.dev/",
         repoURL: "https://github.com/infant-lang"
     }
