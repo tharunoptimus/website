fdb55e512a98706a97bb6109ce00dd61db10b4a8
Tharunraj Optimus <tharunoptimus@outlook.com>
2022-09-08 11:52:52 +0000

    Added stackData

---
+++ pages/components/stackData.ts
@@ -0,0 +1,74 @@
+export interface Stack {
+	imageUrl: string
+	imageAlt: string
+}
+
+export let bootstrap: Stack = {
+	imageUrl: "/assets/stack/bootstrap.svg",
+	imageAlt: "Bootstrap",
+}
+export let css: Stack = {
+	imageUrl: "/assets/stack/css.svg",
+	imageAlt: "CSS",
+}
+export let express: Stack = {
+	imageUrl: "/assets/stack/express.svg",
+	imageAlt: "Express",
+}
+export let go: Stack = {
+	imageUrl: "/assets/stack/go.svg",
+	imageAlt: "Go Lang",
+}
+export let html: Stack = {
+	imageUrl: "/assets/stack/html.svg",
+	imageAlt: "HTML",
+}
+export let jquery: Stack = {
+	imageUrl: "/assets/stack/jquery.svg",
+	imageAlt: "jQuery",
+}
+export let js: Stack = {
+	imageUrl: "/assets/stack/js.svg",
+	imageAlt: "JavaScript",
+}
+export let mongodb: Stack = {
+	imageUrl: "/assets/stack/mongodb.svg",
+	imageAlt: "Mongo DB",
+}
+export let nextjs: Stack = {
+	imageUrl: "/assets/stack/nextjs.svg",
+	imageAlt: "Next JS",
+}
+export let nodejs: Stack = {
+	imageUrl: "/assets/stack/nodejs.svg",
+	imageAlt: "Node JS",
+}
+export let npm: Stack = {
+	imageUrl: "/assets/stack/npm.svg",
+	imageAlt: "NPM",
+}
+export let pug: Stack = {
+	imageUrl: "/assets/stack/pug.svg",
+	imageAlt: "PUG",
+}
+export let pwa: Stack = {
+	imageUrl: "/assets/stack/pwa.svg",
+	imageAlt: "PWA",
+}
+export let react: Stack = {
+	imageUrl: "/assets/stack/react.svg",
+	imageAlt: "React JS",
+}
+export let ts: Stack = {
+	imageUrl: "/assets/stack/ts.svg",
+	imageAlt: "TypeScript",
+}
+export let vscode: Stack = {
+	imageUrl: "/assets/stack/vscode.svg",
+	imageAlt: "VS Code",
+}
+export let websockets: Stack = {
+	imageUrl: "/assets/stack/websockets.svg",
+	imageAlt: "WebSockets",
+}
+
